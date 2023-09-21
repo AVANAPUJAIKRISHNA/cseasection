4948771b0aaa8fae79d36ee18e79519896792fbc
Admin <Admin@DESKTOP-MENGJQL>
2023-09-21 14:03:37 +0530

    commit by team lead

---
+++ src/com/lbrce/cseasection/sample.java
@@ -0,0 +1,12 @@
+package com.lbrce.cseasection;
+
+public class sample {
+	public static void main(String args[])
+	{
+		JK();
+	}
+	public static void JK()
+	{
+		System.out.println("This is Team Lead");
+	}
+}
