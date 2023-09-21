9745ad35e4565fde8f2586269f0fa28cb5d5992e
krishnasai02 <103171387+krishnasai02@users.noreply.github.com>
2023-09-21 14:45:05 +0530

    Revert "commit by sai"

--- src/com/lbrce/cseasection/sample.java
+++ src/com/lbrce/cseasection/sample.java
@@ -4,14 +4,9 @@ public class sample {
 	public static void main(String args[])
 	{
 		JK();
-		Js();
 	}
 	public static void JK()
 	{
 		System.out.println("This is Team Lead");
-	}
-	public static void Js()
-	{
-		System.out.println("This is Team member");
 	}
 }
